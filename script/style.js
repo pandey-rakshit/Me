8cfbcf9ed008cbc5c865e4c50a21cdb5e58d9f82
Rakshit Pandey <pandeyrakshit03@gmail.com>
2020-09-27 12:54:29 +0530

    animation

---
+++ script/style.js
@@ -0,0 +1,16 @@
+function reRun() {
+    document.querySelector('.fa-dragon').classList.remove('spin-animation')
+    document.querySelector('.fa-dragon').classList.remove('move-animation')
+    setTimeout(function() {
+      document.querySelector('.fa-dragon').classList.add('spin-animation')
+    },300)
+  }
+  
+  function move() {
+    document.querySelector('.fa-dragon').classList.remove('spin-animation')
+    document.querySelector('.fa-dragon').classList.remove('move-animation')
+    setTimeout(function() {
+      document.querySelector('.fa-dragon').classList.add('move-animation')
+    },300)
+  }
+  
