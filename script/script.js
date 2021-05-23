e54bed8a12f6e6eb2fdff4e9560856dbb1d1c69f
Rakshit Pandey <45165045+pandey-rakshit@users.noreply.github.com>
2021-05-23 22:30:55 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+var data = document.getElementById("info");
+    function showinfo(){
+        data.style.left="0";
+    }
+    function hideinfo(){
+        data.style.left="-200px";
+
+    }
+
+function theame(){
+    document.querySelector('.fa-dragon').classList.remove('light');
+    document.querySelector('.fa-dragon').classList.remove('dark');
+}
